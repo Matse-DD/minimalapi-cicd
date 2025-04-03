3896f0af832312ec095a301bddcfd6b8bd2ed66e
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-04-03 08:09:00 +0200

    added a role assignment of contributor in terraform

--- ClassLib/obj/Debug/net8.0/ClassLib.AssemblyInfo.cs
+++ ClassLib/obj/Debug/net8.0/ClassLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2295d4cfe94b13c14794a088e0de1be3866f59d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6d61a765a1992f790da105b8677b12332e2787")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
